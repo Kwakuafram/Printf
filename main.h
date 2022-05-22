9fa4e3b7709676951d842d20769989f67ded5d05
Kwakuafram <kwakuafram89@gmail.com>
2022-05-22 03:51:37 -0700

    My first Printf function

--- main.h
+++ main.h
@@ -5,9 +5,22 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int
+ */
+typedef struct structprint
+{
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
+
 int _putchar(char ch);
 int _puts(char *string);
-int prints(char *format, va_list);
+int printc(char *format, va_list);
 int printstr(char *format, va_list);
 int (*driver(char *format))(char *format, va_list);
 int _printf(char *format, ...);
